1cfdc4b6632e85f45652adefaeafe092a2944d9d
sarvesh <sarveshc111@gmail.com>
2024-06-19 21:50:34 +0530

    second

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main{
     public static void main(String [] args){
         System.out.println("This is java program");
         System.out.println(new Date());
+        System.out.println("Changes made in the repository : 1");
     }
 }
